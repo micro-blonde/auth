6b44f31319f37b644ad90fcc2f352d05fe3a8dbb
Saeed Kamyabi <saeed@kamyabi.me>
2023-06-30 11:21:06 +0330

    account base

--- account/account.go
+++ account/account.go
@@ -1,9 +1,15 @@
 package account
 
-type Account[T Model] struct {
+type Base struct {
 	Id uint64
 
 	Status Status
+}
+
+type Account[T Model] struct {
+	Base
+
+	T T `gorm:"embedded" json:",inline"`
 }
 
 func NewAccount[T Model]() *Account[T] {
