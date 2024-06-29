435870b60a72c99ee3285e958807bd02289f6773
Saeed Kamyabi <saeed@kamyabi.me>
2024-06-29 18:07:43 +0330

    proto

---
+++ proto/README.md
@@ -0,0 +1,5 @@
+To generate/update go files run:
+
+```shell script
+./generate.sh
+```
---
+++ proto/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir ./out
+rm -r auth/* || mkdir ./auth
+
+protoc --go_out=./out --go_opt=paths=source_relative --go-grpc_out=./out\
+    --go-grpc_opt=paths=source_relative -I . \
+    proto/auth.proto \
+    proto/account/account.proto \
+    proto/account/profile/profile.proto \
+    proto/account/account_profile.proto \
+    proto/account/request.proto \
+    proto/account/profile/request.proto
+
+mv out/proto/* ./auth
+rm -r ./out
