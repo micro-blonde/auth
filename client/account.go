d716c1f33c332b5423051b042434693923be7f6f
Saeed Kamyabi <saeed@kamyabi.me>
2024-09-09 15:17:13 +0330

    update proto

--- client/account.go
+++ client/account.go
@@ -3,12 +3,13 @@ package client
 import (
 	"context"
 
+	"github.com/micro-blonde/auth/proto/auth"
 	"github.com/micro-blonde/auth/proto/auth/account"
 	"google.golang.org/grpc"
 )
 
 func (c *client) GetAccount(ctx context.Context,
-	in *account.GetRequest, opts ...grpc.CallOption) (*account.Account, error) {
+	in *auth.Request, opts ...grpc.CallOption) (*account.Account, error) {
 	result := new(account.Account)
 	if err := c.ensureService(); err != nil {
 		return result, err
@@ -21,7 +22,7 @@ func (c *client) GetAccount(ctx context.Context,
 }
 
 func (c *client) ListAccounts(ctx context.Context,
-	in *account.ListRequest, opts ...grpc.CallOption) (*account.Accounts, error) {
+	in *auth.Request, opts ...grpc.CallOption) (*account.Accounts, error) {
 	result := new(account.Accounts)
 	if err := c.ensureService(); err != nil {
 		return result, err
--- client/profile.go
+++ client/profile.go
@@ -3,12 +3,13 @@ package client
 import (
 	"context"
 
+	"github.com/micro-blonde/auth/proto/auth"
 	"github.com/micro-blonde/auth/proto/auth/account/profile"
 	"google.golang.org/grpc"
 )
 
-func (c *client) GetProfile(ctx context.Context, in *profile.GetRequest,
-	opts ...grpc.CallOption) (*profile.Profile, error) {
+func (c *client) GetProfile(ctx context.Context,
+	in *auth.Request, opts ...grpc.CallOption) (*profile.Profile, error) {
 	result := new(profile.Profile)
 	if err := c.ensureService(); err != nil {
 		return result, err
@@ -21,7 +22,7 @@ func (c *client) GetProfile(ctx context.Context, in *profile.GetRequest,
 }
 
 func (c *client) ListProfiles(ctx context.Context,
-	in *profile.ListRequest, opts ...grpc.CallOption) (*profile.Profiles, error) {
+	in *auth.Request, opts ...grpc.CallOption) (*profile.Profiles, error) {
 	result := new(profile.Profiles)
 	if err := c.ensureService(); err != nil {
 		return result, err
